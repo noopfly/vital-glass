e5449dff71974a3ad39c2bc47b6aff7565028469
noopfly <lauramelngalve@gmail.com>
2026-07-12 00:34:13 +0300

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./",
+  base: mode === "production" ? "/vital-glass/" : "/",
   server: {
     host: "::",
     port: 8080,
